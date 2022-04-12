9a808522fa1fb776e31154a80928022d17fef676
Josh Marusek <jm553020@ohio.edu>
2022-04-11 21:13:38 -0400

    download project files

---
+++ samplemain.cc
@@ -0,0 +1,18 @@
+/*************************************************************************
+   This is a sample of what the main should like for the first phase of 
+   the Othello game.
+
+   John Dolan	Ohio University		Fall 2019
+*************************************************************************/
+#include <iostream>
+#include "othello.h"
+using namespace std;
+using namespace main_savitch_14;
+
+
+int main(){
+   Othello mygame;
+   mygame.play();
+
+   return 0;
+}
